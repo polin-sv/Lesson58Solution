d126ee8ec5eed89bebb8d4ddc245e1e2e456a8ba
polin-sv <polinasvetlay@icloud.com>
2026-04-27 17:46:43 +0300

    test06

--- task01/task06.cpp
+++ task01/task06.cpp
@@ -10,10 +10,10 @@ int main() {
 	ptr = &number;
 
 	cout << "0)" << ptr << endl;
-	cout << "1)" << ++ptr << endl;
-	cout << "2)" << ++ptr << endl;
-	cout << "3)" << ++ptr << endl;
-	cout << "4)" << ++ptr << endl;
+	cout << "1)" << (ptr += 10) << endl;
+	cout << "2)" << (ptr += 10) << endl;
+	cout << "3)" << (ptr += 10) << endl;
+	cout << "4)" << (ptr += 10) << endl;
 
 
 	return 0;
