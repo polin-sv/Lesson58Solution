edd1366fdee25452cba186b68ed2138f46344487
polin-sv <polinasvetlay@icloud.com>
2026-04-27 17:21:02 +0300

    test3

---
+++ task01/test03.cpp
@@ -0,0 +1,27 @@
+//pointer
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int* ptr_a = NULL;
+	//char* ptr_c = NULL;
+	long long* ptr_l = NULL;
+	float* ptr_f = NULL;
+	double* ptr_d = NULL;
+	bool* ptr_b = NULL;
+	short* ptr_s = nullptr;
+	void* ptr = nullptr;
+
+	cout << "value: " << ptr_a << endl;
+	cout << "value: " << ptr_d << endl;
+	cout << "value: " << ptr_f << endl;
+	//cout << "value: " << ptr_c << endl;
+	cout << "value: " << ptr_l << endl;
+	cout << "value: " << ptr_b << endl;
+	cout << "value: " << ptr_s << endl;
+	cout << "value: " << ptr << endl;
+
+
+	return 0;
+}
