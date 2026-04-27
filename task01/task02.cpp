f6aeceda2ed413596c77e11b364f4ed0ed9e2ad2
polin-sv <polinasvetlay@icloud.com>
2026-04-27 17:14:42 +0300

    task02

---
+++ task01/task02.cpp
@@ -0,0 +1,27 @@
+//pointer
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int* ptr_a;
+	char* ptr_c;
+	long long* ptr_l;
+	float* ptr_f;
+	double* ptr_d;
+	bool* ptr_b;
+	short* ptr_s;
+	void* ptr;
+
+	cout << "size of int is " << sizeof(ptr_a) << endl;
+	cout << "size of double is " << sizeof(ptr_d) << endl;
+	cout << "size of float is " << sizeof(ptr_f) << endl;
+	cout << "size of char is " << sizeof(ptr_c) << endl;
+	cout << "size of long long is " << sizeof(ptr_l) << endl;
+	cout << "size of bool is " << sizeof(ptr_b) << endl;
+	cout << "size of short is " << sizeof(ptr_s) << endl;
+	cout << "size of short is " << sizeof(ptr) << endl;
+
+
+	return 0;
+}
