645b5cb52785e23352ada8a35ec2bf3fe81e8f35
polin-sv <polinasvetlay@icloud.com>
2026-04-27 17:05:29 +0300

    test 01

---
+++ task01/test01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	char c;
+	long long l;
+	float f;
+	double d;
+	bool b;
+	int array[10];
+	short s;
+
+	cout << "size of int is " << sizeof(a) << endl;
+	cout << "size of double is " << sizeof(d) << endl;
+	cout << "size of float is " << sizeof(f) << endl;
+	cout << "size of char is " << sizeof(c) << endl;
+	cout << "size of long long is " << sizeof(l) << endl;
+	cout << "size of bool is " << sizeof(b) << endl;
+	cout << "size of array is " << sizeof(array) << endl;
+	cout << "size of short is " << sizeof(s) << endl;
+
+
+	return 0;
+}
