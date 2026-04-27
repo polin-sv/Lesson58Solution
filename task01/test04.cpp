64e23c7ce2f3ee0a163a56cae95b70e809f3c36a
polin-sv <polinasvetlay@icloud.com>
2026-04-27 17:43:52 +0300

    test05

--- task01/test04.cpp
+++ task01/test04.cpp
@@ -1,22 +1,22 @@
-//pointer
-
-#include <iostream>
-using namespace std;
-
-
-int main() {
-	int* ptr = NULL;
-
-	int number = 10;
-	ptr = &number;
-
-	cout << "before: number = " << number << endl;
-
-	*ptr = *ptr + 5;
-
-	cout << "after: number = " << number << endl;
-	
-
-
-	return 0;
-}
+////pointer
+//
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main() {
+//	int* ptr = NULL;
+//
+//	int number = 10;
+//	ptr = &number;
+//
+//	cout << "before: number = " << number << endl;
+//
+//	*ptr = *ptr + 5;
+//
+//	cout << "after: number = " << number << endl;
+//	
+//
+//
+//	return 0;
+//}
---
+++ task01/test05.cpp
@@ -0,0 +1,20 @@
+////pointer
+//
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main() {
+//	int number = 10;
+//	int* ptr = NULL;
+//	ptr = &number;
+//
+//	cout << "0)" << ptr << endl;
+//	cout << "1)" << ++ptr << endl;
+//	cout << "2)" << ++ptr << endl;
+//	cout << "3)" << ++ptr << endl;
+//	cout << "4)" << ++ptr << endl;
+//
+//
+//	return 0;
+//}
